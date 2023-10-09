783be3ba4da24f3db4d193161b62542527d3843d
hemant <panchalhemant030700@gmail.com>
2023-10-09 20:08:44 +0530

    All done to news Monkey App

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types'
+
 import React from 'react'
 import {
 
@@ -11,7 +11,7 @@ const Navbar=(props)=>{
     return (
 
 
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">NewsMonkey</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
